d0f1bd7cba0baeb54581c74b2b164d414fde3b5b
Nicholas Koukoutsis <nkk800@gmail.com>
2019-02-03 20:48:57 +0200

    Bug fix

--- router.go
+++ router.go
@@ -48,6 +48,9 @@ func (r *Router) BuildMuxRouter() *mux.Router {
 			muxRoute.Methods(route.methods...)
 		}
 		muxRoute = r.MuxRouter.HandleFunc(lessTrailingSlashURL, *route.function)
+		if r.RespondToOptions {
+			route.methods = append(route.methods, "OPTIONS")
+		}
 		if len(route.methods) != 0 {
 			muxRoute.Methods(route.methods...)
 		}
